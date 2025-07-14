efd04a5a8263e27075bb9f6d26e63a09637f0351
pranav-22cs <pranavb0826@gmail.com>
2025-07-14 16:33:50 +0530

    app,jsx button color yellow

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   Button
 </button>
     <h1 className="bg-yellow-100">MealMate</h1>
-       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+       <button class="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
   Button
 </button>
     </>
