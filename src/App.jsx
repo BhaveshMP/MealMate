b577bb08de4052814090200465fca73dad65e3e3
BhaveshMP <patilbhavesh666@gmail.com>
2025-07-14 15:59:57 +0530

    Initial commit for MealMate project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+
+  return (
+    <>
+    <button class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded">
+  Button
+</button>
+    <h1 className="bg-yellow-100">MealMate</h1>
+       <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+  Button
+</button>
+    </>
+  )
+}
+
+export default App
